3a7134739f3bd3d56b9df3b9413bbaaa379af86e
epimelis <epimelis1@gmail.com>
2017-11-20 16:33:03 -0500

    commit

--- sql/sqlnotes_transfer_oracle_to_mysql.sql
+++ sql/sqlnotes_transfer_oracle_to_mysql.sql
@@ -1,5 +1,5 @@
-#################################################################################################################################################
-USEFUL QUERY ON STK TABLES
+--#################################################################################################################################################
+--USEFUL QUERY ON STK TABLES
 select 
 tkr, 
 s.seq, --sma1.seq, sma2.seq, 
@@ -21,8 +21,8 @@ from stk where tkr='MSFT'
 --and to_char(tkr_date, 'yyyy') > 2015
 ORDER BY SEQ
 
-#################################################################################################################################################
-AHMAD APIS
+--#################################################################################################################################################
+--AHMAD APIS
 
 
 testing_stk_trading.get_pending_action(
@@ -74,55 +74,10 @@ exception when others then
 end;
 /
 
-#################################################################################################################################################
-EXPORT/IMPORT
 
 
-create table tempay_stk as select * from stk where tkr='MSFT';
-
-select count(*) from tempay_stk;
---4391
-
-create table tempay_stk_event as select * from stk_event e where exists 
- (select 1 from tempay_stk s where s.stk_id=e.stk_id);
- 
- select count(*) from tempay_stk_event;
- --185
- 
-create table tempay_stk_trade as select * from stk_trade t where exists 
- (select 1 from tempay_stk_event e where e.event_id=t.event_id and tkr='MSFT');
- 
- select count(*) from tempay_stk_trade;
- --65
- 
- SELECT * FROM STRATEGY;
- 
-create table tempay_ind_boll_bands as select * from ind_boll_bands B 
-where exists (select 1 from stk s where s.stk_id=b.stk_id and tkr='MSFT');
-
-select count(*) from tempay_ind_boll_bands; 
---8754
-
-------------------------------------------------------------------------------------------------
-
-drop table stk;
-drop table stk_event;
-drop table stk_trade;
-drop table ind_boll_bands;
-
-
-
-create table stk as select * from tempay_stk;
-
-create table stk_event as select * from tempay_stk_event;
-
-create table stk_trade as select * from tempay_stk_trade;
-
-create table ind_boll_bands as select * from tempay_ind_boll_bands;
-
-
-#################################################################################################################################################
-MYSQL CREATE TABLES 
+--#################################################################################################################################################
+--MYSQL CREATE TABLES
 
 
 show tables;
@@ -195,8 +150,8 @@ CREATE TABLE IND_BOLL_BANDS
 
 
 
-#################################################################################################################################################
-GENERATE INSERT STATEMENTS FOR MYSQL
+--#################################################################################################################################################
+--GENERATE INSERT STATEMENTS FOR MYSQL
 
 CREATE TABLE MYSQL_INSERT (table_name varchar2(30), seq number, insert_str varchar2(4000)); 
 
@@ -204,95 +159,25 @@ select * from stk where stk_id in (104, 1059) order by stk_id;
 select count(*), count(exch), count(tkr), count(per), count(tkr_date), count(open), count(high), count(low), count(close), count(vol), count(seq) from stk;
 
 
-declare
-cursor c0 is
-    select stk_id from stk where stk_id in (104, 1059) order by stk_id;
-cursor c1(c_tab_name varchar2) is
-    select * from user_tab_columns where table_name=c_tab_name order by column_id;
-v_tab_name varchar2(100);
-v_column_name varchar2(100);
-v_insert_str varchar2(8000);
-v_sql varchar2(4000);
-v_value varchar2(500);
-v_cnt number;
-begin
-v_tab_name :='STK';
---==========================================================================================================
---List out column names in insert statement
-for r0 in c0 loop
-    v_insert_str :='insert into ' || v_tab_name || ' (';
-    v_cnt :=0;
-    for r1 in c1(v_tab_name) loop
-        ---------------------------------------------
-        --add comma to separate data vlues
-        if v_cnt>0 then 
-            v_insert_str := v_insert_str || ',';
-        end if;
-        v_insert_str :=v_insert_str || r1.column_name;
-        v_cnt := v_cnt +1;    
-    end loop;
-    v_insert_str :=v_insert_str || ')';
-    --==========================================================================================================
-    v_insert_str :=v_insert_str || ' values (';
-    v_cnt :=0;
-    for r1 in c1(v_tab_name) loop
-        ---------------------------------------------
-        --add comma to separate data vlues
-        if v_cnt>0 then 
-            v_insert_str := v_insert_str || ',';
-        end if;
-        ---------------------------------------------
-        --prepare sql query to get the single data value from that 1 column name
-        if r1.data_type='DATE' then 
-            v_column_name := 'to_char(' || r1.column_name || ',' ||'''yyyy-mm-dd'')';
-        else 
-            v_column_name :=r1.column_name;
-        end if;
-        v_sql :='select ' || v_column_name || ' from ' || v_tab_name || ' where  stk_id=' || r0.stk_id;
-        --dbms_output.put_line(v_sql);
-        ---------------------------------------------
-        --get the data value and format accordingly for insert statement
-        execute immediate v_sql into v_value;
-        if r1.data_type='VARCHAR2' then
-            v_value := '''' || v_value || '''';
-        /* 
-        -- for Oracle date inserts
-        elsif r1.data_type='DATE' then -- only for inserts into Oracle DB 
-            v_value := 'to_date(''' || v_value || ''')';
-         */
-        -- for MySQL date inserts
-        elsif r1.data_type='DATE' then -- only for inserts into Oracle DB 
-            v_value := '''' || v_value || '''';
-        else
-            null; -- do nothing
-        end if; 
-        --dbms_output.put_line(v_value);
-        v_insert_str :=v_insert_str || v_value;
-        v_cnt := v_cnt +1;    
-    end loop;
-    v_insert_str :=v_insert_str || ');';
-    dbms_output.put_line(v_insert_str);
-end loop;    
-exception when others then 
-    dbms_output.put_line(sqlerrm);
-end;
-
-++++++++++++++++++++++++
-
-CREATE TABLE MYSQL_INSERT (table_name varchar2(30), seq number, insert_str varchar2(4000)); 
+--------------PLSQL-------------------
 
 
-select * from stk where stk_id in (104, 1059) order by stk_id;
-select count(*), count(exch), count(tkr), count(per), count(tkr_date), count(open), count(high), count(low), count(close), count(vol), count(seq) from stk;
+select INSERT_STR from mysql_insert
+WHERE 1=1
+AND table_name='STK';
 
-select * from mysql_insert;
 
-select * from stk_trade;
 
 declare
+/*
 cursor c0 is
     select stk_id from stk
-    where stk_id <2000 
+    where TKR='MSFT'
+    order by stk_id;
+*/
+cursor c0 is
+    select stk_id from stk_event e
+    where exists (select 1 from stk s where s.stk_id=e.stk_id and tkr='MSFT')
     order by stk_id;
 cursor c1(c_tab_name varchar2) is
     select * from user_tab_columns where table_name=c_tab_name order by column_id;
@@ -304,8 +189,12 @@ v_value varchar2(500);
 v_cnt number;
 v_seq number;
 begin
+/*
 delete from mysql_insert where table_name='STK';
 v_tab_name :='STK';
+*/
+delete from mysql_insert where table_name='STK_EVENT';
+v_tab_name :='STK_EVENT';
 v_seq :=0;
 --==========================================================================================================
 --List out column names in insert statement
@@ -316,11 +205,11 @@ for r0 in c0 loop
     for r1 in c1(v_tab_name) loop
         ---------------------------------------------
         --add comma to separate data vlues
-        if v_cnt>0 then 
+        if v_cnt>0 then
             v_insert_str := v_insert_str || ',';
         end if;
         v_insert_str :=v_insert_str || r1.column_name;
-        v_cnt := v_cnt +1;    
+        v_cnt := v_cnt +1;
     end loop;
     v_insert_str :=v_insert_str || ')';
     --==========================================================================================================
@@ -329,14 +218,14 @@ for r0 in c0 loop
     for r1 in c1(v_tab_name) loop
         ---------------------------------------------
         --add comma to separate data vlues
-        if v_cnt>0 then 
+        if v_cnt>0 then
             v_insert_str := v_insert_str || ',';
         end if;
         ---------------------------------------------
         --prepare sql query to get the single data value from that 1 column name
-        if r1.data_type='DATE' then 
+        if r1.data_type='DATE' then
             v_column_name := 'to_char(' || r1.column_name || ',' ||'''yyyy-mm-dd'')';
-        else 
+        else
             v_column_name :=r1.column_name;
         end if;
         v_sql :='select ' || v_column_name || ' from ' || v_tab_name || ' where  stk_id=' || r0.stk_id;
@@ -346,27 +235,27 @@ for r0 in c0 loop
         execute immediate v_sql into v_value;
         if r1.data_type='VARCHAR2' then
             v_value := '''' || v_value || '''';
-        /* 
+        /*
         -- for Oracle date inserts
-        elsif r1.data_type='DATE' then -- only for inserts into Oracle DB 
+        elsif r1.data_type='DATE' then -- only for inserts into Oracle DB
             v_value := 'to_date(''' || v_value || ''')';
          */
         -- for MySQL date inserts
-        elsif r1.data_type='DATE' then -- only for inserts into Oracle DB 
+        elsif r1.data_type='DATE' then -- only for inserts into Oracle DB
             v_value := '''' || v_value || '''';
         else
             null; -- do nothing
-        end if; 
+        end if;
         --dbms_output.put_line(v_value);
         v_insert_str :=v_insert_str || v_value;
-        v_cnt := v_cnt +1;    
+        v_cnt := v_cnt +1;
     end loop;
     v_insert_str :=v_insert_str || ');';
     --dbms_output.put_line(v_insert_str);
     insert into mysql_insert(table_name, seq, insert_str) values(v_tab_name, v_seq, v_insert_str);
-end loop;    
+end loop;
 commit;
-exception when others then 
+exception when others then
     dbms_output.put_line(sqlerrm);
 end;
 
